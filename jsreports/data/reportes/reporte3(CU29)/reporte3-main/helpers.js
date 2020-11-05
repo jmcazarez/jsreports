c9b224881e80085ab812750a9fd80152cfd55f32
adsum <adsum@localhost.localdomain>
2020-11-05 16:55:43 -0700

    actualizado sprint 6 funcional

---
+++ jsreports/data/reportes/reporte3(CU29)/reporte3-main/helpers.js
@@ -0,0 +1,14 @@
+function now() {
+    var date = new Date();
+    var day = date.getDate();
+    if (day < 10) {
+        day = '0' +  day;
+    }
+    var month = date.getMonth() + 1;
+    if (month < 10) {
+        month = '0' +  month;
+    }
+    var year = date.getFullYear();
+
+    return day + '/' + month + '/' + year;
+}
--- jsreports/data/reportes/reporte4(CU30)/reporte4-main/helpers.js
+++
@@ -1,9 +0,0 @@
-function getPageNumber (pageIndex) {
-    if (pageIndex == null) {
-        return ''
-    }
-    
-    const pageNumber = pageIndex + 1
-    
-    return pageNumber
-}
